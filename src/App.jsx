19b8a7e1fac85d82b33f9a8e9a3035502e6df067
Zoe Weinstein <mxzweinstein@gmail.com>
2022-07-27 19:14:16 -0600

    simple drag and drop test

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {DndContext} from '@dnd-kit/core';
+
+import {Droppable} from './components/Droppable';
+import {Draggable} from './components/Draggable';
+
+export default function App() {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = (
+    <Draggable>Drag me</Draggable>
+  );
+  
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>
+        {isDropped ? draggableMarkup : 'Drop here'}
+      </Droppable>
+    </DndContext>
+  );
+  
+  function handleDragEnd(event) {
+    if (event.over && event.over.id === 'droppable') {
+      setIsDropped(true);
+    }
+  }
+}
--- src/components/Droppable.jsx
+++ src/components/Droppable.jsx
@@ -1,32 +1,18 @@
-import { useDroppable } from "@dnd-kit/core";
-import { horizontalListSortingStrategy, SortableContext } from "@dnd-kit/sortable";
-import SortableItem from "./SortableItem";
-import React from "react";
+import React from 'react';
+import {useDroppable} from '@dnd-kit/core';
 
-const Droppable = ({ id, items }) => {
-  const { setNodeRef } = useDroppable({ id });
-
-  const droppableStyle = {
-    padding: "20px 10px",
-    margin: "0px 0px 20px 20px",
-    border: "1px solid black",
-    borderRadius: "5px",
-    minWidth: 300,
-    minHeight: 110,
-    flexWrap: "wrap",
-    
-    
+export function Droppable(props) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: 'droppable',
+  });
+  const style = {
+    color: isOver ? 'green' : undefined,
   };
-
+  
+  
   return (
-    <SortableContext id={id} items={items} strategy={horizontalListSortingStrategy}>
-      <div ref={setNodeRef} style={droppableStyle}>
-        {items.map((item) => (
-          <SortableItem key={item} id={item} />
-        ))}
-      </div>
-    </SortableContext>
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
   );
-};
-
-export default Droppable;
+}
